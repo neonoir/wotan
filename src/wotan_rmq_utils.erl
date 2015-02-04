33d3d9b905391ea8c5c8c12b67bd26b54d4a9352
Kamyar Navidan <kamyar.n@gmail.com>
2015-02-05 03:19:45 +0330

    add functions worker_queue, assign_task

--- src/wotan_rmq_utils.erl
+++ src/wotan_rmq_utils.erl
@@ -3,29 +3,27 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
-
+-spec get_channel(string()) -> {ok, pid()} | {error, Error}.
 get_channel(Host) ->
     case amqp_connection:start(#amqp_params_network{host = Host}) of
 	{ok, Connection} ->
 	    case amqp_connection:open_channel(Connection) of
-		{ok, Channel} ->
+		Channel ->
 		    Channel;
-		_ ->
-		    channel_error
+		{error, Error} ->
+		    {error, Error}
 	    end;
-	_ ->
-	    connection_error
+	{error, Error} ->
+	    {error, Error}
     end.
-
 
 exchange_declare(Channel, Exchange, Type) ->
     amqp_channel:call(Channel, 
 		      #'exchange.declare'{exchange = Exchange, type = Type}).
 
-queue_declare(Channel) ->
-    #'queue.declare_ok'{queue = Queue} = amqp_channel:call(
-					   Channel, 
-					   #'queue.declare'{exclusive = true}),
+queue_declare(Channel, Method) ->
+    #'queue.declare_ok'{queue = Queue} = 
+	amqp_channel:call(Channel, Method),
     Queue.
 
 queue_bind(Channel, Exchange, Queue) ->
@@ -48,3 +46,20 @@ subscribe(Channel, Queue) ->
 
 ack(Channel, Tag) ->
     amqp_channel:cast(Channel, #'basic.ack'{delivery_tag = Tag}).
+
+worker_queue(Channel, QueueName) ->
+    #'queue.declare_ok'{queue = Queue} =
+        amqp_channel:call(Channel, 
+			  #'queue.declare'{queue = QueueName,
+					   durable = true}),
+    amqp_channel:call(Channel, #'basic.qos'{prefetch_count = 1}),
+    Queue.
+
+assign_task(Channel, Msg) ->    
+    amqp_channel:cast(Channel,
+                      #'basic.publish'{
+			 exchange = <<"">>,
+			 routing_key = <<"wotan_task_queue">>},
+                      #amqp_msg{
+			 props = #'P_basic'{delivery_mode = 2},
+			 payload = Msg}).

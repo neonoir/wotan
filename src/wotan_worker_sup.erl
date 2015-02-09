1ad023d3c312cfb8b829e9c6259c024060f13367
Kamyar Navidan <kamyar.n@gmail.com>
2015-02-09 18:17:23 +0330

    add supervisor

---
+++ src/wotan_worker_sup.erl
@@ -0,0 +1,51 @@
+%%%-------------------------------------------------------------------
+%%% @author Kamyar Navidan <kamyar.n@gmail.com>
+%%% @copyright (C) 2015, Kamyar Navidan
+%%% @doc
+%%%
+%%% @end
+%%% Created :  8 Feb 2015 by Kamyar Navidan <kamyar.n@gmail.com>
+%%%-------------------------------------------------------------------
+-module(wotan_worker_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+start_child() ->
+    supervisor:start_child(?SERVER, []).
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+init([]) ->
+    RestartStrategy = simple_one_for_one,
+    MaxRestarts = 0,
+    MaxSecondsBetweenRestarts = 1,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = brutal_kill,
+    Type = worker,
+
+    WorkerChild = {wotan_worker, {wotan_wroker, start_link, []},
+		   Restart, Shutdown, Type, [wotan_wroker]},
+
+    {ok, {SupFlags, [WorkerChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================

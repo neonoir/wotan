f2197a5c3bef49fb4e9a74c19c85ea92b46752b0
Kamyar Navidan <kamyar.n@gmail.com>
2015-02-13 08:55:29 +0330

    add test job module (for now)

---
+++ src/test_job.erl
@@ -0,0 +1,26 @@
+-module(test_job).
+
+-export([fun1/0,
+	 fun2/0,
+	 fun3/1]).
+
+-include("wotan.hrl").
+
+fun1() ->
+    [#task{mod = ?MODULE, 
+	  func = fun2,
+	  args = []}].
+
+fun2() ->
+    [#task{mod = ?MODULE, 
+	  func = fun3,
+	  args = [1]},
+     #task{mod = ?MODULE, 
+	   func = fun3,
+	   args = [4]},
+    #task{mod = ?MODULE, 
+	  func = fun3,
+	  args = [asd]}].
+
+fun3(_) ->
+    [].

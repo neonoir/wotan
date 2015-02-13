706bc9947b76a1d2799689383cd4ec22483b13c6
Kamyar Navidan <kamyar.n@gmail.com>
2015-02-13 08:52:47 +0330

    minor fix for wotan headers

--- src/wotan.hrl
+++ src/wotan.hrl
@@ -1,16 +1,18 @@
+-include_lib("amqp_client/include/amqp_client.hrl").
+
 -record(task, {
-	  module :: module(),
-	  function :: function(),
-	  arguments :: [any()]}).
+	  mod :: module(),
+	  func :: function(),
+	  args :: [any()]}).
 -type task() :: #task{}.
--record(taskmsg_id, {job_id :: atom(), 
-		     task_id :: atom()}).
+-record(taskmsg_id, {job_id :: binary(), 
+		     task_id :: binary()}).
 -type taskmsg_id() :: #taskmsg_id{}.
 -record(taskmsg, {taskmsg_id :: taskmsg_id(), 
 		  task :: task(),
 		  status :: assigned | done | error}).
 -type taskmsg() :: #taskmsg{}.
 -record(job, {
-	  id :: atom(),
+	  job_id :: binary(),
 	  tasks :: [task()]}).
 -type job() :: #job{}.
